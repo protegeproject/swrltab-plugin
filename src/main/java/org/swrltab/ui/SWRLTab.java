37f9a4624f0725b82c796f586ab0972e871db06a
martin <martin@zippyrate.com>
2014-06-29 15:43:22 -0700

    More plugin wiring

--- src/main/java/org/swrltab/ui/SWRLTab.java
+++ src/main/java/org/swrltab/ui/SWRLTab.java
@@ -2,12 +2,23 @@ package org.swrltab.ui;
 
 import java.awt.BorderLayout;
 
+import javax.swing.Icon;
+
 import org.apache.log4j.Logger;
 import org.protege.editor.owl.model.OWLModelManager;
 import org.protege.editor.owl.model.event.EventType;
 import org.protege.editor.owl.model.event.OWLModelManagerChangeEvent;
 import org.protege.editor.owl.model.event.OWLModelManagerListener;
 import org.protege.editor.owl.ui.OWLWorkspaceViewsTab;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.swrlapi.core.SWRLAPIFactory;
+import org.swrlapi.core.SWRLAPIOWLOntology;
+import org.swrlapi.core.SWRLRuleEngine;
+import org.swrlapi.drools.DroolsFactory;
+import org.swrlapi.drools.DroolsSWRLRuleEngineCreator;
+import org.swrlapi.ui.controller.SWRLAPIApplicationController;
+import org.swrlapi.ui.model.SWRLAPIApplicationModel;
+import org.swrlapi.ui.view.rules.SWRLAPIRulesView;
 
 public class SWRLTab extends OWLWorkspaceViewsTab
 {
@@ -27,10 +38,32 @@ public class SWRLTab extends OWLWorkspaceViewsTab
 	{
 		super.initialise();
 
-		this.modelManager = getOWLModelManager();
-		this.modelManager.addListener(listener);
+		// Get the active OWL ontology
+		OWLOntology ontology = this.modelManager.getActiveOntology();
+
+		// Create a SWRLAPI OWL ontology from the active OWL ontology
+		SWRLAPIOWLOntology swrlapiOWLOntology = SWRLAPIFactory.createSWRLAPIOWLOntology(ontology);
+
+		// Create a Drools-based query engine
+		SWRLRuleEngine ruleEngine = SWRLAPIFactory.createSQWRLQueryEngine(swrlapiOWLOntology,
+				new DroolsSWRLRuleEngineCreator());
+
+		// Create the application model, supplying it with the ontology and rule engine
+		SWRLAPIApplicationModel applicationModel = SWRLAPIFactory.createSWRLAPIApplicationModel(swrlapiOWLOntology,
+				ruleEngine);
+
+		// Create the application controller
+		SWRLAPIApplicationController applicationController = SWRLAPIFactory
+				.createSWRLAPIApplicationController(applicationModel);
+
+		Icon ruleEngineIcon = DroolsFactory.getSWRLRuleEngineIcon();
+		SWRLAPIRulesView rulesView = new SWRLAPIRulesView(applicationController, ruleEngineIcon);
 
 		setLayout(new BorderLayout());
+		add(rulesView);
+
+		this.modelManager = getOWLModelManager();
+		this.modelManager.addListener(listener);
 
 		log.info("SWRLTab initialized");
 	}
